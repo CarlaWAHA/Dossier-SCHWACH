56c94b14338a43659367411248fadafb08a55059
Carla Waha <estellewaha@gmail.com>
2025-06-03 20:58:15 +0200

    finalisation de pointandclick

--- frontend/src/Views/Hotel.vue
+++ frontend/src/Views/Hotel.vue
@@ -71,6 +71,96 @@ export default {
       revealedPassword: '',
       scenes: [
         {
+          img: '/hotel/images/face_ascenseur1.jpg',
+          sound: '',
+          text: "C'était une longue route je vais me reposer dans ma chambre",
+          hotspots: [
+            { x: 73, y: 70, width: 2, height: 6, targetScene: 1, sound: '/hotel/sons/elevatordoor.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/face_ascenseur2.jpg',
+          sound: '',
+          text: "Cet ascenseur m'a jamais inspiré confiance...",
+          autoAdvance: 5000
+        },
+        {
+          img: '/hotel/images/face_ascenseur3.jpg',
+          sound: '/hotel/sons/step.wav.mp3',
+          text: "Il faut que j'aille au 1er étage",
+          hotspots: [
+            { x: 46, y: 30, width: 15, height: 65, targetScene: 3, sound: '/hotel/sons/step.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/panneau_ascenseur1.jpg',
+          sound: '/hotel/sons/elevator.wav',
+          text: "Je devrai vivement aller me reposer",
+          hotspots: [
+            { x: 38.5, y: 28.5, width: 4, height: 8, targetScene: 4, sound: '/hotel/sons/elevator.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/panneau_ascenseur2.jpg',
+          sound: '/hotel/sons/fin.mp3',
+          text: "Il me semble avoir laissé des médicaments pour mes troubles de sommeil",
+          autoAdvance: 5000
+        },
+        {
+          img: '/hotel/images/attente_ascenseur.jpg',
+          sound: '',
+          text: "Je les ai laissés dans le tiroir de la table de nuit",
+          autoAdvance: 5000
+        },
+        {
+          img: '/hotel/images/attente_ascenseur2.jpg',
+          sound: '/hotel/sons/step.wav.mp3',
+          text: "Tiens ? La femmme de ménage habituelle n'est pas là ? Comment elle s'appellait déjà...",
+          hotspots: [
+            { x: 55, y: 30, width: 10, height: 55, targetScene: 7, sound: '/hotel/sons/step.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/porte.jpg',
+          sound: '/hotel/sons/keys.wav',
+          text: "C'est étrange que toutes les affaires de ménage ont étés laissées dans le couloir...",
+          hotspots: [
+            { x: 28, y: 29, width: 8, height: 16, targetScene: 8, sound: '/hotel/sons/keys.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/portevuepres.jpg',
+          sound: '/hotel/sons/dooropen.wav',
+          text: "...",
+          hotspots: [
+            { x: 35, y: 80, width: 10, height: 20, targetScene: 9, sound: '/hotel/sons/dooropen.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/vue_meuble.jpg',
+          sound: '/hotel/sons/step.wav.mp3',
+          text: "Je prends mes cachets et au dodo",
+          hotspots: [
+            { x: 36, y: 40, width: 10, height: 20, targetScene: 10, sound: '/hotel/sons/step.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/meuble.jpg',
+          sound: '/hotel/sons/drawer.wav',
+          text: "",
+          hotspots: [
+            { x: 47, y: 70, width: 10, height: 20, targetScene: 11, sound: '/hotel/sons/drawer.wav' }
+          ]
+        },
+        {
+          img: '/hotel/images/meuble_ouvert.jpg',
+          sound: '/hotel/sons/lumieredisjoncte.mp3',
+          text: "Mes cachets ne sont plus là",
+          hotspots: [
+            { x: 30, y: 70, width: 40, height: 20, targetScene: 12, sound: '/hotel/sons/lumieredisjoncte.mp3' }
+          ]
+        },
+        {
           img: '/hotel/images/chambrenuit.png',
           sound: '/hotel/sons/fin.mp3',
           text: 'Il y a quelque chose qui arrive...',
